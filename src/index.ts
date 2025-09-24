fe637cc927145665c765c504d3545a662f6cc065
Alejandro Diez Bermejo <alejandrodiezbermejo@gmail.com>
2025-09-24 18:49:17 +0200

    Install and configure Cloudflare

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 
-const app = new Hono();
+const app = new Hono<{ Bindings: Env }>();
 
 app.get("/", (ctx) => ctx.text("Mi Roulotte"));
 
-serve({ fetch: app.fetch, port: 3010 }, (info) => {
-    console.log("\x1b[42;1m hono \x1b[0m\n");
-    console.log(`\x1b[90m┃\x1b[0m Local    \x1b[36mhttp://localhost:${info.port}/\x1b[0m\n`);
-});
+export default app;

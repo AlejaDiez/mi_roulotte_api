87c5188630655c58bfe57b5841ec7ef39b555cb2
Alejandro Diez Bermejo <alejandrodiezbermejo@gmail.com>
2025-10-02 16:21:26 +0200

    Add auth routes

---
+++ src/controllers/auth/verify_two_factor_auth.ts
@@ -0,0 +1,6 @@
+import { HTTPException } from "hono/http-exception";
+import { Handler } from "hono/types";
+
+export const verifyTwoFactorAuth: Handler = async (ctx) => {
+    throw new HTTPException(501, { message: "This feature is not implemented yet" });
+};

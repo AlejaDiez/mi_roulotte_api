f3da64d03cfc9eba1496fe63f8ba5db272610111
Alejandro Diez Bermejo <alejandrodiezbermejo@gmail.com>
2025-10-04 17:15:51 +0200

    Add profile endpoints

--- src/controllers/auth/edit_profile.ts
+++ src/controllers/auth/edit_profile.ts
@@ -1,6 +1,52 @@
+import { UsersTable } from "@db/schemas";
+import { EditUser, User } from "@models/auth";
+import { hash } from "@utils/crypto";
+import { filterColumns } from "@utils/filter_object";
+import { DrizzleQueryError } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/d1";
+import { eq } from "drizzle-orm/sql";
 import { HTTPException } from "hono/http-exception";
 import { Handler } from "hono/types";
+import { ZodError } from "zod";
 
 export const editProfile: Handler<Env> = async (ctx) => {
-    throw new HTTPException(501, { message: "This feature is not implemented yet" });
+    const fields = ctx.req.query("fields")?.split(",");
+    const columns = {
+        id: UsersTable.id,
+        username: UsersTable.username,
+        email: UsersTable.email,
+        role: UsersTable.role,
+        isActive: UsersTable.isActive,
+        emailVerified: UsersTable.emailVerified,
+        twoFactorAuthentication: UsersTable.twoFactorAuthentication,
+        createdAt: UsersTable.createdAt,
+        updatedAt: UsersTable.updatedAt
+    };
+
+    try {
+        const body = EditUser.parse(await ctx.req.json());
+        const query = drizzle(ctx.env.DB)
+            .update(UsersTable)
+            .set({
+                username: body.username,
+                password: body.password ? await hash(body.password) : undefined
+            })
+            .where(eq(UsersTable.id, ctx.var.uid))
+            .returning(filterColumns(columns, fields));
+
+        // Update user
+        const data: any = await query.get();
+
+        return ctx.json(User.parse(data), 200);
+    } catch (err) {
+        if (err instanceof ZodError)
+            throw new HTTPException(422, {
+                message: err.issues.map(({ path, message }) => `${path} ${message}`).join("; ")
+            });
+        else if (err instanceof DrizzleQueryError)
+            throw new HTTPException(409, {
+                message: "Impossible to update profile"
+            });
+        throw err;
+    }
 };
--- src/controllers/auth/get_profile.ts
+++ src/controllers/auth/get_profile.ts
@@ -1,6 +1,30 @@
-import { HTTPException } from "hono/http-exception";
+import { UsersTable } from "@db/schemas";
+import { User } from "@models/auth";
+import { filterColumns } from "@utils/filter_object";
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/d1";
 import { Handler } from "hono/types";
 
 export const getProfile: Handler<Env> = async (ctx) => {
-    throw new HTTPException(501, { message: "This feature is not implemented yet" });
+    const fields = ctx.req.query("fields")?.split(",");
+    const columns = {
+        id: UsersTable.id,
+        username: UsersTable.username,
+        email: UsersTable.email,
+        role: UsersTable.role,
+        isActive: UsersTable.isActive,
+        emailVerified: UsersTable.emailVerified,
+        twoFactorAuthentication: UsersTable.twoFactorAuthentication,
+        createdAt: UsersTable.createdAt,
+        updatedAt: UsersTable.updatedAt
+    };
+    const query = drizzle(ctx.env.DB)
+        .select(filterColumns(columns, fields))
+        .from(UsersTable)
+        .where(eq(UsersTable.id, ctx.var.uid));
+
+    // Get user date
+    const data = await query.get();
+
+    return ctx.json(User.parse(data), 200);
 };

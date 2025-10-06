0f7ff18a6b3a384a08b6a8ea750b0b7362cc1578
Alejandro Diez Bermejo <alejandrodiezbermejo@gmail.com>
2025-10-06 18:30:14 +0200

    Add comments routes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import authRoutes from "@routes/auth";
+import commentsRoutes from "@routes/comments";
 import filesRoutes from "@routes/files";
 import stagesRoutes from "@routes/stages";
 import tripsRoutes from "@routes/trips";
@@ -7,6 +8,7 @@ import { Hono } from "hono";
 const app = new Hono<Env>();
 
 app.route("/auth", authRoutes);
+app.route("/comments", commentsRoutes);
 app.route("/files", filesRoutes);
 app.route("/trips", tripsRoutes);
 app.route("/trips/:trip_id/stages", stagesRoutes);
